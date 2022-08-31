8d69307f383badf8168b37cea3cef35e544505e3
Farhan Ahmed <iamfarhan09@gmail.com>
2022-08-31 12:10:55 +0600

    form text fix

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Container, Form } from "react-bootstrap";
-import './Login.css'
+import { Link } from "react-router-dom";
+import "./Login.css";
 const Login = () => {
   return (
     <Container>
@@ -9,23 +10,29 @@ const Login = () => {
       <div className="loginForm">
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
+            <Form.Label>Your Email</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
+            <Form.Label>Your Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
+          <Form.Group
+            className="mb-3"
+            controlId="formBasicCheckbox"
+          ></Form.Group>
           <Button variant="primary" type="submit">
             Submit
           </Button>
+          <Form.Text className="text-muted">
+            <Link className="forgetPassword" to="/forget">
+              Forget Password?{" "}
+            </Link>
+            <Link className="adminLink" to="/admin">
+              Contact Admin
+            </Link>
+          </Form.Text>
         </Form>
       </div>
     </Container>
