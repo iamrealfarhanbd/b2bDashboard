0bb7503e198e45b4681f1be58a9440dc4a23fe2d
Farhan Ahmed <iamfarhan09@gmail.com>
2022-09-01 13:20:56 +0600

    dashboard layout ready

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './app.css'
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import Login from "./Components/Login/Login";
@@ -5,10 +6,14 @@ import Login from "./Components/Login/Login";
 function App() {
   return (
     <>
-       <Routes>
-        <Route path="Dashboard" element={<Dashboard />} />
-        <Route path="/" element={<Login />} />
-      </Routes>
+      <div className="App">
+        <div className="AppGlass">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="Login" element={<Login />} />
+          </Routes>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/Components/Dashboard/SidebarData.js
@@ -0,0 +1,18 @@
+// Sidebar imports
+import {
+  UilEstate,
+  UilChart,
+} from "@iconscout/react-unicons";
+
+// Sidebar Data
+export const SidebarData = [
+  {
+    icon: UilEstate,
+    path: '/',
+    heading: "Dashboard",
+  },
+  {
+    icon: UilChart,
+    heading: 'Setting'
+  },
+];
